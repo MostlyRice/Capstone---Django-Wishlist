6e734328b4a7a7a7df4e5ffe2175591c711ee1c8
MostlyRice <amazinzygy@gmail.com>
2023-04-01 14:20:00 -0500

    finished for now

--- wishlist/travel_wishlist/urls.py
+++ wishlist/travel_wishlist/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 from . import views
 
-
-urlpatterns = [  
-    path('', views.place_list, name='place_list'),
-    path('visited', views.places_visited, name='places_visited'),
-    path('about', views.about, name='about'),
-
+urlpatterns = [
+    path('', views.place_list, name='place_list'), # Path leads to place_list function/page in views.py
+    path('about', views.about, name='about'), # Path leads to about function/page in views.py
+    path('visited', views.places_visited, name='places_visited'), # Path leads to visited function/page in views.py
+    path('place/<int:place_pk>/was_visited', views.place_was_visited, name='place_was_visited') # Path will lead to view that will update a place object as visited
 ]
--- wishlist/travel_wishlist/views.py
+++ wishlist/travel_wishlist/views.py
@@ -1,28 +1,42 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Place
 from .forms import NewPlaceForm
 
-# Create your views here.
 
-def place_list(request):
+def place_list(request): 
 
     if request.method == 'POST':
+        # Create new place
+        form = NewPlaceForm(request.POST) # Create form from data in request
+        place = form.save() # Create new place object from data entered in form
+        if form.is_valid(): # DB constraint validation
+            place.save() # Save model to DB
+            return redirect('place_list') # Refresh home page to display newly added places
 
-        form = NewPlaceForm(request.POST)
-        place = form.save()
-        if form.is_valid():
-            place.save()
-            return redirect('place_list')
+    places = Place.objects.filter(visited=False).order_by('name') # Retrieve all objects from database where visited is False, order by name
+    new_place_form = NewPlaceForm() # Empty form used to create HTML
+    # Combine the template with dictionary containing places list and the place form to create a web page containing them together
+    return render(request, 'travel_wishlist/wishlist.html', {'places': places, 'new_place_form': new_place_form}) 
 
-    places = Place.objects.filter(visited=True).order_by('name')
-    new_place_form = NewPlaceForm()
-    return render(request,'travel_wishlist/wishlist.html', {'places':places, 'new_place_form': new_place_form})
-
-def places_visited(request):
-    visited = Place.objects.filter(visited=False)
-    return render(request, 'travel_wishlist/visted.html', {'visited':visited})
 
 def about(request):
-    author = 'James'
-    about = 'A website to create a list of places to visit'
-    return render(request, 'travel_wishlist/about.html', {'author':author, 'about':about})
+    # About page containing author and description
+    author = 'Natalie'
+    about = 'A website to create a list of places to visit.'
+    return render(request, 'travel_wishlist/about.html', {'author': author, 'about': about})
+
+
+def places_visited(request):
+    # Page containing list of places that have already been visited
+    visited = Place.objects.filter(visited=True) # Get all Place objects that have been checked as visited
+    return render(request, 'travel_wishlist/visited.html', {'visited': visited})
+
+
+def place_was_visited(request, place_pk):
+    # Allows user to change the value of a place object's 'visited' attribute to True
+    if request.method == 'POST':
+        place = get_object_or_404(Place, pk=place_pk) # Get the object that corresponds to the requested primary key - send 404 response if pk doesn't exist
+        place.visited = True # Change the object's visited value to True
+        place.save()
+
+    return redirect('place_list') # Reload to update list of places on home page
