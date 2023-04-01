a62e8735cc513a18d2833e86504c8c4d4597a83e
MostlyRice <amazinzygy@gmail.com>
2023-04-01 12:27:09 -0500

    added forms and created an about me html

--- wishlist/travel_wishlist/urls.py
+++ wishlist/travel_wishlist/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [  
     path('', views.place_list, name='place_list'),
+    path('about', views.about, name='about'),
 
 ]
--- wishlist/travel_wishlist/views.py
+++ wishlist/travel_wishlist/views.py
@@ -1,8 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Place
+from .forms import NewPlaceForm
 
 # Create your views here.
 
 def place_list(request):
+
+    if request.method == 'POST':
+
+        form = NewPlaceForm(request.POST)
+        place = form.save()
+        if form.is_valid():
+            place.save()
+            return redirect('place_list')
+
     places = Place.objects.filter(visited=False).order_by('name')
-    return render(request,'travel_wishlist/wishlist.html', {'places':places})
+    new_place_form = NewPlaceForm()
+    return render(request,'travel_wishlist/wishlist.html', {'places':places, 'new_place_form': new_place_form})
+
+def about(request):
+    author = 'James'
+    about = 'A website to create a list of places to visit'
+    return render(request, 'travel_wishlist/about.html', {'author':author, 'about':about})
